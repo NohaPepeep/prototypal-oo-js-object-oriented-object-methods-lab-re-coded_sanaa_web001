9bbe7280dc54fac92e45f5497014a2b84feb7564
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-08-21 08:33:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ BoardMember.prototype.doCharity=function(){
   return "I like to help people.";
 }
 
-BoardMember.prototype.doCharity=function(){
+BoardMember.prototype.releasePressStatement=function(){
   return "I like to help people.";
 }
